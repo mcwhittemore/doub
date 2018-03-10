4d9a12434d8c3bccad09ee2dad67b8f2503467c1
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2018-03-10 16:14:29 -0500

    needs work

--- doub.cpp
+++ doub.cpp
@@ -3,6 +3,12 @@
 
 int main(int argc, char **argv)
 {
+
+  if (argv[1] == "--version") {
+    std::cout << "cli: 0.2.0\ndoubscipt: 0.1.0\n";
+    exit(0);
+  }
+
   try {
     
     doub::Doubscript prog = doub::Doubscript::fromFile(argv[1]);
